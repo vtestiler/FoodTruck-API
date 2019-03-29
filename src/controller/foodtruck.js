11cb9406bf90779f82da3b945ffeef135449bb09
vladimir testiler <vtestiler@yahoo.com>
2019-03-29 14:41:45 -0400

    Created routes for search of trucks by id, foodtype and reviews

--- src/controller/foodtruck.js
+++ src/controller/foodtruck.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { Router} from 'express';
 import FoodTruck from '../model/foodtruck';
+import Review from '../model/review';
 import bodyParser from 'body-parser';
 
 
@@ -13,6 +14,10 @@ export default ({ config, db}) => {
   api.post('/add', (req, res) => {
     let newFoodTruck = new FoodTruck();
     newFoodTruck.name = req.body.name;
+    newFoodTruck.foodtype = req.body.foodtype;
+    newFoodTruck.avgcost = req.body.avgcost;
+    newFoodTruck.geometry.coordinates = req.body.geometry.coordinates;
+  //newFoodTruck.reviews = req.body.reviews;
 
     newFoodTruck.save(err => {
       if(err) {
@@ -25,17 +30,20 @@ export default ({ config, db}) => {
   });
 
   // '/v1/foodtruck' - Read
+  // get list of all foodtrucks
   api.get('/', (req, res) => {
     FoodTruck.find({}, (err, foodtrucks) => {
       if(err) {
         res.send(err);
       }
+      //res.json({ message: 'List of FoodTrucks:'});
       res.json(foodtrucks);
     });
   });
 
-  // '/v1/foodtruck/:id' - Read 1
-  api.get('/:id', (req, res) => {
+  // 'v1/foodtruck/id/:id' - Read 1
+  //get foodrtuck by id
+  api.get('/id/:id', (req, res) => {
     FoodTruck.findById(req.params.id, (err, foodtruck) => {
       if(err){
         res.send(err);
@@ -51,11 +59,14 @@ export default ({ config, db}) => {
         res.send(err);
       }
       foodtruck.name = req.body.name;
+      foodtruck.foodtype = req.body.foodtype;
+      foodtruck.avgcost = req.body.avgcost;
+      foodtruck.geometry.coordinates = req.body.geometry.coordinates;
       foodtruck.save(err => {
         if(err) {
           res.send(err);
         }
-        res.json({ message: "FoodTruck info updated"});
+        res.json({ message: `FoodTruck - ${req.params.id} info updated`});
       });
     });
   });
@@ -72,6 +83,69 @@ export default ({ config, db}) => {
     });
 
   });
+
+// add a review for a specific foodtruck id
+// '/v1/foodtruck/reviews/add/:id'
+api.post('/reviews/add/:id', (req, res) => {
+  FoodTruck.findById(req.params.id, (err, foodtruck) =>{
+    if (err) {
+      res.send(err);
+    }
+    let newReview = new Review();
+
+    newReview.title = req.body.title;
+    newReview.text = req.body.text;
+    newReview.foodtruck = foodtruck._id;
+    newReview.save((err, review) =>{
+      if (err){
+        res.send(err);
+      }
+      foodtruck.reviews.push(newReview);
+      foodtruck.save(err => {
+        if (err) {
+          res.send(err);
+        }
+        res.json({ message: 'Food Truck review saved.'});
+      });
+    });
+  });
+});
+
+// get reviews from a specific truck id
+// 'v1/foodtruck/reviews/:id' Read
+
+api.get('/reviews/:id', (req, res) => {
+  Review.find({foodtruck: req.params.id} , (err, reviews) => {
+    if(err){
+      res.send(err);
+    }
+    res.json(reviews);
+  });
+});
+
+//get list of all reviews
+// 'v1/foodtruck/reviews' Read
+api.get('/reviews', (req, res) => {
+  Review.find({} , (err, reviews) => {
+    if(err){
+      res.send(err);
+    }
+    res.json(reviews);
+  });
+});
+
+
+// get list of foodtrucks with certian foodtype
+// '/v1/foodtruck/foodtype/:foodtype' - Read
+api.get('/foodtype/:foodtype', (req, res) => {
+
+  FoodTruck.find({foodtype: req.params.foodtype}, (err, foodtrucks) => {
+    if(err) {
+      res.send(err);
+    }
+    res.json(foodtrucks);
+  });
+});
 
 
   return api;
--- src/model/foodtruck.js
+++ src/model/foodtruck.js
@@ -1,8 +1,23 @@
 import mongoose from 'mongoose';
+import Review from './review';
+
 let Schema = mongoose.Schema;
 
-let foodtruckSchema = new Schema({
-  name: String
+let FoodTruckSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  foodtype: {
+    type: String,
+    required: true
+  },
+  avgcost: Number,
+  geometry: {
+    type: {type: String, default: 'Point'},
+    coordinates: [Number]
+  },
+  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]
 });
 
-module.exports = mongoose.model('FoodTruck', foodtruckSchema);
+module.exports = mongoose.model('FoodTruck', FoodTruckSchema);
